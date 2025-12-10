aaa2b675646c6d2ca9906546f88674df70c072cd
med363 <blibechmohamedamine97@gmail.com>
2025-12-10 13:23:50 +0100

    composant amiliore 3 new message

--- higherordercomponent/src/App.js
+++ higherordercomponent/src/App.js
@@ -10,6 +10,11 @@ const GoodbyeWorld = ({farewellMessage}) => {
   return <h2>{farewellMessage}</h2>;
 }
 
+//Definition Component
+const DefHoc = ({message}) => {
+  return <p>{message}</p>;
+}
+
 
 //HOC
 const withDefaultMessage = (WrappedComponent, message="Hello, World!", farewellMessage="Goodbye, World!") => {
@@ -17,16 +22,25 @@ const withDefaultMessage = (WrappedComponent, message="Hello, World!", farewellM
     return <WrappedComponent {...props} message={message} farewellMessage={farewellMessage} />;
   };
 };
+
+//Usage of HOC Definition
+const withDefHoc = (WrappedComponent, message) => {
+  return (props) => {
+    return <WrappedComponent {...props} message={message} />;
+  };
+};
+
 //Enhanced Component using HOC
 const EnhancedHelloWorld = withDefaultMessage(HelloWorld , "Welcome to the HOC Example!");
 const EnhancedGoodbyeWorld = withDefaultMessage(GoodbyeWorld , "See you later!");
-
+const EnhancedDefComponent = withDefHoc(DefHoc, "HOC est une fonction qui prend un composant en entrée et renvoie un nouveau composant avec des fonctionnalités supplémentaires.");
 //App Component
 function App() {
   return (
     <div className="App">
       <EnhancedHelloWorld />
       <EnhancedGoodbyeWorld />
+      <EnhancedDefComponent />
     </div>
   );
 }
