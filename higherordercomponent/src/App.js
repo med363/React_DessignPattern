d77d2910742f322de81a00ea9e9116536ec21aa3
med363 <blibechmohamedamine97@gmail.com>
2025-12-10 11:43:23 +0100

    composant amiliore 2

--- higherordercomponent/src/App.js
+++ higherordercomponent/src/App.js
@@ -5,21 +5,28 @@ const HelloWorld = ({message}) => {
   return <h1>{message}</h1>;
 };
 
+//component2
+const GoodbyeWorld = ({farewellMessage}) => {
+  return <h2>{farewellMessage}</h2>;
+}
+
 
 //HOC
-const withDefaultMessage = (WrappedComponent, message="Hello, World!") => {
+const withDefaultMessage = (WrappedComponent, message="Hello, World!", farewellMessage="Goodbye, World!") => {
   return (props) => {
-    return <WrappedComponent {...props} message={message} />;
+    return <WrappedComponent {...props} message={message} farewellMessage={farewellMessage} />;
   };
 };
 //Enhanced Component using HOC
 const EnhancedHelloWorld = withDefaultMessage(HelloWorld , "Welcome to the HOC Example!");
+const EnhancedGoodbyeWorld = withDefaultMessage(GoodbyeWorld , "See you later!");
 
 //App Component
 function App() {
   return (
     <div className="App">
       <EnhancedHelloWorld />
+      <EnhancedGoodbyeWorld />
     </div>
   );
 }
